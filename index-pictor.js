8ff512bc8ddb8614345d5eac68cc5c3c32ddd6b6
Navish Kumar <smartdata.nav@gmail.com>
2026-05-28 12:47:58 +0530

    Change logs

--- index-pictor.js
+++ index-pictor.js
@@ -459,6 +459,7 @@ const tcpServer = net.createServer(socket => {
                         const rawPhone = unescaped.slice(4, 10)
                             .map(b => `${(b >> 4) & 0x0F}${b & 0x0F}`).join('');
                         console.log('[AUTH] raw BCD phone:', rawPhone, 'stripped:', rawPhone.replace(/^0/,''));
+                        console.log('[AUTH] raw bytes:', unescaped.slice(4, 10).toString('hex'));
                         socket.write(buildAck(phone, seq, msgId));
                         socket.write(buildVideoRequest(phone, CONFIG.serverIp, CONFIG.tcpPort, 1));
                         tcpSockets[phone] = socket;
